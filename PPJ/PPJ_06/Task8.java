6d1611be58d63597ca8e21f00352121ca434576c
Wiktor <wiko700@gmail.com>
2023-11-06 22:43:09 +0100

    Changed variable name

--- PPJ/PPJ_06/Task8.java
+++ PPJ/PPJ_06/Task8.java
@@ -16,20 +16,20 @@ public class Task8 {
         System.out.println("B bottom-right corner: [" + points[6] + "," + points[7] + "]");
 
         int biggestXOfTLCorners = points[0] > points[4] ? points[0] : points[4];
-        int biggestXOfBRCorners = points[2] > points[6] ? points[6] : points[2];
-        int biggestYOfTLCorners = points[1] > points[5] ? points[5] : points[1];
+        int smallestXOfBRCorners = points[2] > points[6] ? points[6] : points[2];
+        int smallestYOfTLCorners = points[1] > points[5] ? points[5] : points[1];
         int biggestYOfBRCorners = points[3] > points[7] ? points[3] : points[7];
 
-        if (biggestXOfBRCorners > biggestXOfTLCorners && biggestYOfTLCorners > biggestYOfBRCorners) {
+        if (smallestXOfBRCorners > biggestXOfTLCorners && smallestYOfTLCorners > biggestYOfBRCorners) {
             System.out.println("C is rectangle, which is created by common part of recntangles A and B");
-            System.out.println("C top-left corner: [" + biggestXOfTLCorners + "," + biggestYOfTLCorners + "]");
-            System.out.println("C bottom-right corner: [" + biggestXOfBRCorners + "," + biggestYOfBRCorners + "]");
+            System.out.println("C top-left corner: [" + biggestXOfTLCorners + "," + smallestYOfTLCorners + "]");
+            System.out.println("C bottom-right corner: [" + smallestXOfBRCorners + "," + biggestYOfBRCorners + "]");
 
             int x = 0;
             int y = 0;
 
-            for (int i = biggestYOfTLCorners; i > biggestYOfBRCorners; i--) x += 1;
-            for (int i = biggestXOfBRCorners; i > biggestXOfTLCorners; i--) y += 1;
+            for (int i = smallestYOfTLCorners; i > biggestYOfBRCorners; i--) x += 1;
+            for (int i = smallestXOfBRCorners; i > biggestXOfTLCorners; i--) y += 1;
 
 
             System.out.println("Area of C rectangle: " + (x * y));
