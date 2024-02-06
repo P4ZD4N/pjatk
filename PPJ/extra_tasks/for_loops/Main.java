15e52eabb716d45ac2a25fb415854872ea482990
Wiktor <wiko700@gmail.com>
2024-02-06 20:37:53 +0100

    Modified Main.java

--- PPJ/extra_tasks/for_loops/Main.java
+++ PPJ/extra_tasks/for_loops/Main.java
@@ -1,10 +1,5 @@
-import tasks.MultiplicationTable;
-import tasks.drawings.Diamond;
-import tasks.drawings.Pyramid;
-import tasks.drawings.Rectangle;
-
 public class Main {
     public static void main(String[] args) {
-        Diamond.print(5, 5);
+
     }
 }
--- PPJ/extra_tasks/while_loops/Main.java
+++ PPJ/extra_tasks/while_loops/Main.java
@@ -1,10 +1,5 @@
-import tasks.GuessNumber;
-import tasks.RandomNumbers;
-import tasks.Sum;
-
 public class Main {
     public static void main(String[] args) {
-        RandomNumbers.generateTenFromOneTo(5);
-        RandomNumbers.generateTwentyFromOneTo(10);
+
     }
 }
