f2700293e31bfec12b80bddfa57446a58b0fbdb6
Wiktor <wiko700@gmail.com>
2024-05-14 23:10:28 +0200

    Added MenuPanelCenter class

---
+++ GUI/Project2/src/panels/MenuPanelCenter.java
@@ -0,0 +1,23 @@
+package panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuPanelCenter extends JPanel {
+
+    private MenuPanelCenterLeft menuPanelCenterLeft;
+    private MenuPanelCenterRight menuPanelCenterRight;
+
+    public MenuPanelCenter() {
+
+        setLayout(new GridLayout(1, 2));
+
+        setBackground(Color.BLACK);
+
+        menuPanelCenterRight = new MenuPanelCenterRight();
+        menuPanelCenterLeft = new MenuPanelCenterLeft(menuPanelCenterRight);
+
+        add(menuPanelCenterLeft);
+        add(menuPanelCenterRight);
+    }
+}
