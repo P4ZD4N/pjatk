54537bc1fd1783651639cf0c4f3ead467d2ee2e4
Wiktor <wiko700@gmail.com>
2024-05-14 23:12:41 +0200

    Added MenuShipPanel class

---
+++ GUI/Project2/src/panels/MenuShipPanel.java
@@ -0,0 +1,13 @@
+package panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MenuShipPanel extends JPanel {
+
+    public MenuShipPanel() {
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        setBackground(Color.BLACK);
+    }
+}
