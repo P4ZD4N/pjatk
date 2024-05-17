ba5eea3c9fc89f00d803fc41cd611aeff91ba963
Wiktor <wiko700@gmail.com>
2024-05-17 18:29:18 +0200

    Added Bullet class

---
+++ GUI/Project2/src/game/Bullet.java
@@ -0,0 +1,29 @@
+package game;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Bullet extends JLabel {
+
+    private static final int SPEED = 1;
+
+    public Bullet(int x, int y) {
+
+        setBounds(x, y, 5, 5);
+        setBackground(Color.GREEN);
+        setOpaque(true);
+    }
+
+    public void move() {
+
+        setLocation(getX(), getY() - SPEED);
+    }
+
+    public boolean hit(Enemy enemy) {
+
+        Rectangle bulletBounds = getBounds();
+        Rectangle enemyBounds = enemy.getBounds();
+
+        return bulletBounds.intersects(enemyBounds);
+    }
+}
