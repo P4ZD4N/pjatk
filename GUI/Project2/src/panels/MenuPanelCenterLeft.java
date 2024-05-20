c8446318f48512e331265d6349adabc85e1569bb
Wiktor <wiko700@gmail.com>
2024-05-20 12:14:08 +0200

    Added menuPanelButtons field and getter

--- GUI/Project2/src/panels/MenuPanelCenterLeft.java
+++ GUI/Project2/src/panels/MenuPanelCenterLeft.java
@@ -5,12 +5,20 @@ import java.awt.*;
 
 public class MenuPanelCenterLeft extends JPanel {
 
+    private MenuPanelButtons menuPanelButtons;
+
     public MenuPanelCenterLeft(MenuPanelCenterRight menuPanelCenterRight) {
 
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.BLACK);
 
         add(Box.createVerticalStrut(50));
-        add(new MenuPanelButtons(menuPanelCenterRight));
+
+        menuPanelButtons = new MenuPanelButtons(menuPanelCenterRight);
+        add(menuPanelButtons);
+    }
+
+    public MenuPanelButtons getMenuPanelButtons() {
+        return menuPanelButtons;
     }
 }
